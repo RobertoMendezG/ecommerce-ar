70759efcfa4461cc5fe28ede86651605c8339a42
roberto <robertocarlosmg10@gmail.com>
2024-09-26 21:58:21 -0600

    creacion de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+import './App.css'
+
+function App() {
+  return (
+    <div className="overflow-x-hidden">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
