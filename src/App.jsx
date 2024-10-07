8b292969b2028e3c3d8558721f37a058a64315d5
RobertoMendezG <rexertops101528@gmail.com>
2024-10-07 16:08:32 -0600

    acyualizacion de codigo

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage/>} />
-          <Route path="/pages/DetailProduct.jsx" element={<DetailProduct/>} />
+          <Route path="/*" element={<DetailProduct/>} />
           
         </Routes>
         </Router>      
