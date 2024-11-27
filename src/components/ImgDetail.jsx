77b6fde626cea5e0ebc53e68d724acd04f40a7c9
roberto <robertocarlosmg10@gmail.com>
2024-11-26 21:07:30 -0600

    Agrego actualizacion de imagen detalle

--- src/components/ImgDetail.jsx
+++ src/components/ImgDetail.jsx
@@ -33,7 +33,7 @@ const ImgDetail = ({ productId }) => {
         {items ? (
             <>
             <div className="col-span-4">
-            <img src={items.imagen} alt="" className="ml-5" width={"300px"} height={"100px"} />
+            <img src={items.imagen} alt="" className="ml-8" width={"300px"} height={"100px"} />
             
         </div>
         </>
