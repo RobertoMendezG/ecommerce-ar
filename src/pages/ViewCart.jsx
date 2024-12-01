0e33d563cdd8b9d37e0c426e861c136623e6d3d2
PerlaAlbores <alboresp575@gmail.com>
2024-11-30 18:13:48 -0600

    Update ViewCart.jsx

--- src/pages/ViewCart.jsx
+++ src/pages/ViewCart.jsx
@@ -2,7 +2,6 @@
 
 import Navbar from "../components/Navbar";
 import CartView from "../components/CartView";
-import Footer from "../components/Footer";
 import Banner from "../components/Banner";
 
 function ViewCart() {
